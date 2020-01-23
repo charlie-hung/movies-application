02b1bf0ff35aa8321ba6fec5e2fb34e2ce2948d7
Hung Ly <hunghly@gmail.com>
2020-01-23 14:29:28 -0600

    created grid, added some styling, changed rating to be stars

--- src/api.js
+++ src/api.js
@@ -1,7 +1,142 @@
-module.exports = {
-  getMovies: () => {
-    return fetch('/api/movies')
-      .then(response => response.json());
+const $ = require('jquery');
 
-  }
+// VARIABLE DECLARATIONS
+let selectedMovie = {
+    id: 0
+};
+
+const getMovies = () => {
+    return fetch('/api/movies')
+        .then(response => {
+            return response.json();
+        });
+};
+
+/*-----Creates an 'li' string for each movie-----*/
+const createMovieString = (moviesObj) => {
+    let htmlString = '';
+    moviesObj.forEach((movie) => {
+        htmlString += `<li class="single-movie-container">
+                    <div class="small-details-container">
+                        <p class="movie-title">${movie.title}</p>
+                        <p class="movie-rating">${generateStars(movie.rating)}</p>
+                    </div>
+                    <div class="small-image-container">
+                        <img src="http://placeholder.pics/svg/100x150" alt="">
+                    </div>
+                </li>`
+    });
+    return htmlString;
+};
+
+const generateStars = (movieRating) => {
+    let starString = "";
+    for (let i = 0; i < movieRating; i ++) {
+        starString += '<i class="fas fa-star"></i>';
+    }
+    return starString;
+};
+
+const addMovie = () => {
+    let addMovieTitleEl = $('#add-movie-title');
+    if ($(addMovieTitleEl).val()) {
+        let movie = {
+            title: $(addMovieTitleEl).val(),
+            rating: $('#add-movie-rating').val()
+        };
+        fetch('/api/movies', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(movie)
+        }).then((data) => {
+            getMovies().then((movies) => {
+                $('.movie-list').html(createMovieString(movies));
+            });
+        })
+    }
+};
+
+const editMovie = () => {
+    let movieChanges = {
+        title: $('#edit-movie-title').val(),
+        rating: $('#edit-movie-rating').val()
+    };
+    console.log(movieChanges);
+    fetch(`/api/movies/${selectedMovie.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(movieChanges)
+    }).then((data) => {
+        console.log(data.json());
+        getMovies().then((movies) => {
+            $('.movie-list').html(createMovieString(movies));
+        });
+    });
+};
+
+const deleteMovie = () => {
+    fetch(`/api/movies/${selectedMovie.id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((data) => {
+        console.log(data.json());
+        getMovies().then((movies) => {
+            $('.movie-list').html(createMovieString(movies));
+        });
+    });
+};
+// SEARCH MOVIE FUNCTIONALITY //
+const searchMovies = () => {
+    let searchResults = [];
+    getMovies().then((movies) => {
+        if ($('#movie-search').val()) {
+            movies.forEach((movie) => {
+                if (movie.title.toLowerCase().includes($('#movie-search').val().toLowerCase())) searchResults.push(movie);
+            });
+        }
+        renderSearchResults(searchResults);
+    })
+};
+
+const renderSearchResults = (results) => {
+    let htmlString = "";
+    results.forEach((result) => htmlString += `<li class="search-result-item" movie-id="${result.id}">${result.title}</li>`);
+    $('.search-results-list').html(htmlString);
+    $('.search-result-item').click(function () {
+        selectedMovie.id = $(this).attr('movie-id');
+        $('#edit-movie-title').val($(this).html());
+    });
+};
+
+const toggleDisable = () => {
+    $('#add-button').attr('disabled', (index, attr) => {
+        return attr === 'disabled' ? null : 'disabled';
+    });
+    $('#search-button').attr('disabled', (index, attr) => {
+        return attr === 'disabled' ? null : 'disabled';
+    });
+    $('#edit-button').attr('disabled', (index, attr) => {
+        return attr === 'disabled' ? null : 'disabled';
+    });
+    $('#delete-button').attr('disabled', (index, attr) => {
+        return attr === 'disabled' ? null : 'disabled';
+    });
+};
+
+module.exports = {
+    selectedMovie,
+    getMovies,
+    createMovieString,
+    addMovie,
+    editMovie,
+    deleteMovie,
+    searchMovies,
+    renderSearchResults,
+    toggleDisable
 };
--- src/index.js
+++ src/index.js
@@ -1,92 +1,16 @@
 {
     const $ = require('jquery');
-    const {getMovies} = require('./api.js');
+    const {selectedMovie, getMovies, createMovieString, addMovie, editMovie, deleteMovie, searchMovies, renderSearchResults, toggleDisable} = require('./api.js');
 
     $(document).ready(() => {
-        // VARIABLE DECLARATIONS
-        let selectedMovie = {
-            id: 0
-        };
-
-
+        $('.movie-list').html('Loading...');
         getMovies().then((movies) => {
-            console.log('Here are all the movies:');
             $('.movie-list').html(createMovieString(movies));
-            Promise.resolve();
+            Promise.resolve().then(response => "Success: " + response);
         }).catch((error) => {
             alert('Oh no! Something went wrong.\nCheck the console for details.');
             console.log(error);
         });
-        /*-----Creates an 'li' string for each movie-----*/
-        let createMovieString = (moviesObj) => {
-            let htmlString = '';
-            moviesObj.forEach((movie) => {
-                htmlString += `<li class="single-movie">
-                <p class="movie-title">${movie.title}</p>
-                <p class="movie-rating">${movie.rating}</p>
-                <p class="movie-id">${movie.id}</p>
-                </li>`
-            });
-            return htmlString;
-        };
-
-        let addMovie = () => {
-            if ($('#add-movie-title').val()) {
-                let movie = {
-                    title: $('#add-movie-title').val(),
-                    rating: $('#add-movie-rating').val()
-                };
-                // console.log(movie);
-                fetch('/api/movies', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(movie)
-                }).then((data) => {
-                    getMovies().then((movies) => {
-                        $('.movie-list').html(createMovieString(movies));
-                    });
-                })
-            }
-        };
-
-        // SEARCH MOVIE FUNCTIONALITY //
-        let searchMovies = () => {
-            let searchResults = [];
-            getMovies().then((movies) => {
-                if ($('#movie-search').val()) {
-                    movies.forEach((movie) => {
-                        if (movie.title.toLowerCase().includes($('#movie-search').val().toLowerCase())) searchResults.push(movie);
-                    });
-                }
-                renderSearchResults(searchResults);
-            })};
-
-        let renderSearchResults = (results) => {
-            let htmlString = "";
-            results.forEach((result)=> htmlString += `<li class="search-result-item" movie-id="${result.id}">${result.title}</li>`);
-            $('.search-results-list').html(htmlString);
-            $('.search-result-item').click(function () {
-               selectedMovie.id = $(this).attr('movie-id');
-               $('#edit-movie-title').val($(this).html());
-            });
-        };
-
-        let toggleDisable = () => {
-          $('#add-button').attr('disabled', (index, attr) => {
-              return attr === 'disabled' ? null : 'disabled';
-          });
-          $('#search-button').attr('disabled', (index, attr) => {
-              return attr === 'disabled' ? null : 'disabled';
-          });
-          $('#edit-button').attr('disabled', (index, attr) => {
-              return attr === 'disabled' ? null : 'disabled';
-          });
-          $('#delete-button').attr('disabled', (index, attr) => {
-              return attr === 'disabled' ? null : 'disabled';
-          });
-        };
 
         //EVENT LISTENERS
         $('#add-button').click((e) => {
@@ -100,37 +24,12 @@
         });
 
         $('#edit-button').click(()=> {
-            let movieChanges = {
-                title: $('#edit-movie-title').val(),
-                rating: $('#edit-movie-rating').val()
-            };
-            console.log(movieChanges);
-            fetch(`/api/movies/${selectedMovie.id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(movieChanges)
-            }).then((data) => {
-                console.log(data.json());
-                getMovies().then((movies) => {
-                    $('.movie-list').html(createMovieString(movies));
-                });
-            });
+            editMovie();
         });
 
+
         $('#delete-button').click(()=> {
-            fetch(`/api/movies/${selectedMovie.id}`, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }).then((data) => {
-                console.log(data.json());
-                getMovies().then((movies) => {
-                    $('.movie-list').html(createMovieString(movies));
-                });
-            });
+           deleteMovie();
         });
 
         $('#movie-search').keyup(() => {
